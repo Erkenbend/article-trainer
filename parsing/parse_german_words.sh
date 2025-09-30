246fbd96be9904d6c46f6bdf4899f7ed7479261a
Erkenbend <ad.game-adress@hotmail.fr>
2025-09-30 21:05:42 +0200

    prepare new word maps in ts

--- parsing/parse_german_words.sh
+++ parsing/parse_german_words.sh
@@ -8,20 +8,26 @@ jq_filter_string() {
   echo ".[] | select((.gender == \"$1\") and (.cefr_level as \$l | $difficulties | index(\$l))) | select(.word | length <= $maxLength) | .word"
 }
 
-echo ${all_words} | jq -r "$(jq_filter_string "masculine" "A1,A2")" > beginnerWordsWithDer.tmp
+wrap_in_quotes() {
+  while IFS=$'\n' read -r in; do
+    echo "\"$in\","
+  done
+}
+
+echo ${all_words} | jq -r "$(jq_filter_string "masculine" "A1,A2")" | wrap_in_quotes > beginnerWordsWithDer.tmp
 echo "Parsed $(wc -l < beginnerWordsWithDer.tmp) beginner words with DER"
 
-echo ${all_words} | jq -r "$(jq_filter_string "masculine" "B1,B2")" > intermediateWordsWithDer.tmp
+echo ${all_words} | jq -r "$(jq_filter_string "masculine" "B1,B2")" | wrap_in_quotes > intermediateWordsWithDer.tmp
 echo "Parsed $(wc -l < intermediateWordsWithDer.tmp) intermediate words with DER"
 
-echo ${all_words} | jq -r "$(jq_filter_string "masculine" "C1,C2")" > expertWordsWithDer.tmp
+echo ${all_words} | jq -r "$(jq_filter_string "masculine" "C1,C2")" | wrap_in_quotes > expertWordsWithDer.tmp
 echo "Parsed $(wc -l < expertWordsWithDer.tmp) expert words with DER"
 
-echo ${all_words} | jq -r "$(jq_filter_string "neuter" "A1,A2")" > beginnerWordsWithDas.tmp
+echo ${all_words} | jq -r "$(jq_filter_string "neuter" "A1,A2")" | wrap_in_quotes > beginnerWordsWithDas.tmp
 echo "Parsed $(wc -l < beginnerWordsWithDas.tmp) beginner words with DAS"
 
-echo ${all_words} | jq -r "$(jq_filter_string "neuter" "B1,B2")" > intermediateWordsWithDas.tmp
+echo ${all_words} | jq -r "$(jq_filter_string "neuter" "B1,B2")" | wrap_in_quotes > intermediateWordsWithDas.tmp
 echo "Parsed $(wc -l < intermediateWordsWithDas.tmp) intermediate words with DAS"
 
-echo ${all_words} | jq -r "$(jq_filter_string "neuter" "C1,C2")" > expertWordsWithDas.tmp
+echo ${all_words} | jq -r "$(jq_filter_string "neuter" "C1,C2")" | wrap_in_quotes > expertWordsWithDas.tmp
 echo "Parsed $(wc -l < expertWordsWithDas.tmp) expert words with DAS"
